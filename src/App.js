5e4fbf111d72597be50ece2a5afa92a7f61dcc27
shagufta <shagufta.batool8@gmail.com>
2021-12-14 11:25:55 +0100

    adding url navgiation tests and using react router library for routing in React

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from 'react'
-import Hello from './Hello';
-import Header from './Header';
-import Footer from './Footer';
+import React from "react";
+import Hello from "./Hello";
+import Header from "./Header";
+import Footer from "./Footer";
+import About from "./About";
+import Projects from "./Projects";
+import { Routes, Route } from "react-router-dom";
 
-export const App = () => {
+const App = () => {
   return (
     <>
-      <Header/>
-      <Hello/>
-      <Footer/>
+       <Header/>
+      <Routes> 
+        <Route exact path="/" element={<Hello/>}/>
+        <Route exact path="/about" element={<About/>}/>
+        <Route exact path="/projects" element={<Projects/>}/>
+      </Routes>
+      <Footer />
     </>
   );
 };
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,20 +1,31 @@
-import React from 'react'
+import React from "react";
 import { Menu, Segment } from "semantic-ui-react";
+import { NavLink, Link } from "react-router-dom";
 
 export const Header = () => {
-    return (
-        <Segment inverted>
-        <Menu inverted secondary>
-            
-            <Menu.Item
-            id ='header'
-            name='my portfolio'
-            
-            />
-           
-            </Menu>
-
+  return (
+    <Segment inverted>
+      <Menu inverted secondary>
+        <Menu.Item
+          id="header"
+          name="my portfolio"
+          as={Link}
+          to={{ pathname: "/" }}
+        />
+        <Menu.Item
+          id="about-tab"
+          name="about me"
+          as={NavLink}
+          to={{ pathname: "/about" }}
+        />
+        <Menu.Item
+          id="projects-tab"
+          name="my porjects"
+          as={NavLink}
+          to={{ pathname: "/projects" }}
+        />
+      </Menu>
     </Segment>
-    );
+  );
 };
 export default Header;
